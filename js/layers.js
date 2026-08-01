97187b7e3cdf6448e711c691fef9aba2f8eda83e
dougie06 <2172936737@qq.com>
2026-08-01 22:38:47 +0800

    8.1 突然有新思路，制作planB

--- js/layers.js
+++ js/layers.js
@@ -1,7 +1,8 @@
 function getClickGain() {
     let base = new Decimal(1);
-
+    
     if (hasUpgrade("c1", 11)) base = base.mul(upgradeEffect("c1", 11));
+    if (hasMilestone("c1", 0)) base = base.mul(1.5);
     return base;
 }
 
@@ -18,6 +19,7 @@ addLayer("c1", {
     row: 0,
     displayRow: 0, 
     color: "#3f2500",
+    resource: "mL可乐",
     layerShown(){return true},
 
     startData() { return {
@@ -30,19 +32,13 @@ addLayer("c1", {
             content: [
                 "main-display",
                 ["display-text", () => `你有 ${format(player.points)} mL 可乐`],
-                ["display-text", () => `你有 ${format(player.c1.points)} 金币`],
+                ["display-text", () => `点击获取 +${format(getClickGain())} 金币`],
+                "blank",
+                "clickables",
                 "blank",
                 "buyables",
                 "blank",
                 "upgrades",
-            ]
-        },
-        "金币": {
-            content: [
-                ["display-text", () => `你有 ${format(player.c1.points)} 金币`],
-                ["display-text", () => `点击获取 +${format(getClickGain())} 金币`],
-                "blank",
-                "clickables",
             ]
         },
         "里程碑": {
@@ -81,9 +77,10 @@ addLayer("c1", {
         display() {
             let cost = this.cost(getBuyableAmount(this.layer, this.id))
             let gain = new Decimal(300)
-            if (hasUpgrade("c1", 12)) gain = gain.add(200)
-            if (hasUpgrade("c1", 14)) gain = gain.add(500)
-            return `花费 ${format(cost)} 金币<br>当前一次购买可获得 ${format(gain)} mL`
+            if (hasUpgrade("c1", 12)) gain = gain.add(hasUpgrade("c1", 12))
+            let amount = getBuyableAmount(this.layer, this.id)
+            
+            return `花费 ${format(cost)} 金币<br>当前一次购买可获得 ${format(gain)} mL<br>已购买瓶数: ${format(amount)}`
         },
         canAfford() { return player[this.layer].points.gte(this.cost(getBuyableAmount(this.layer, this.id))) },
         buy() {
@@ -96,17 +93,17 @@ addLayer("c1", {
     }},
 
     milestones: {
-        11: {
+        0: {
             requirementDescription: "拥有1000mL可乐",
-            effectDescription: "解锁Cola Concentrate",
+            effectDescription: "尝到甜头，金币获取×1.5",
             done() { return player.points.gte(1000) }
         }
     },
     
     upgrades: {
         11: {
-            title: "喝300mL可乐",
-            description: "你渴望喝下更多Cola,可乐以0.03倍提升金币获取",
+            title: "跃跃欲试",
+            description: "你渴望更多Cola,可乐以0.03倍提升金币获取",
             cost: new Decimal(300),
             currencyDisplayName: "mL",
             currencyInternalName: "points",
@@ -116,30 +113,35 @@ addLayer("c1", {
             unlocked() { return true }
         },
         12: {
-            title: "喝1L可乐",
-            description: "你不再满足于300mL的Cola,可乐获取量增加200mL",
+            title: "一口下去不满足",
+            description: "可乐获取量增加300mL",
             cost: new Decimal(1000),
             currencyDisplayName: "mL",
             currencyInternalName: "points",
-            effect() { return new Decimal(0.5) ;},
+            effect() { return new Decimal(300) ;},
             unlocked() { return hasUpgrade("c1", 11); }
         },
         13: {
-            title: "喝2.5L可乐",
-            description: "你开始寻找批发商,可乐价格上涨乘数降低为1.2",
-            cost: new Decimal(5000),
+            title: "可乐批发商",
+            description: "可乐价格上涨乘数降低到1.2",
+            cost: new Decimal(2500),
             currencyDisplayName: "mL",
             currencyInternalName: "points",
             effect() { return new Decimal(1.2) ;},
             unlocked() { return hasUpgrade("c1", 12); }
         },
         14: {
-            title: "喝5L可乐",
-            description: "更大的可乐包装，可乐获取量增加500mL",
+            title: "回收可乐瓶",
+            description: "基于购买可乐次数提升金币获取",
             cost: new Decimal(5000),
             currencyDisplayName: "mL",
             currencyInternalName: "points",
-            effect() { return new Decimal(500) ;},
+            effect() {
+                let amount = getBuyableAmount(this.layer, 11)
+                let eff = new Decimal(amount).pow(0.1).max(1)
+                return eff
+            },
+            effectDisplay() { return `x${format(this.effect())}` },
             unlocked() { return hasUpgrade("c1", 13); }
         }
     }     
